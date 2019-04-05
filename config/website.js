1e2c37c5c96f9172b996894e21400f32b7bc215d
Matt Waters <matthewdw89@gmail.com>
2019-04-05 11:16:54 -0400

    Initial commit

---
+++ config/website.js
@@ -0,0 +1,25 @@
+const tailwind = require('../tailwind')
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Matthew Waters, Full-Stack Developer', // Navigation and Site Title
+  siteTitleAlt: 'Matthew Waters, Front-End Web Developer', // Alternative Site title for SEO
+  siteTitleShort: 'Matthew Waters', // short_name for manifest
+  siteHeadline: 'Creating marvelous web & blazginly fast websites', // Headline for schema.org JSONLD
+  siteUrl: 'https://matthewh20.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  author: 'LekoArts', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  // userTwitter: '@cara', // Twitter Username
+  ogSiteName: 'Matt', // Facebook Site Name
+  ogLanguage: 'en_US', // Facebook Language
+  googleAnalyticsID: 'UA-39299313-3',
+
+  // Manifest and Progress color
+  themeColor: tailwind.colors.orange,
+  backgroundColor: tailwind.colors.blue,
+}
