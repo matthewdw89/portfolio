e3931ff074f86b5755c297369999f5b410b0d085
Matt Waters <matthewdw89@gmail.com>
2019-04-12 10:47:28 -0400

    reduced the size of project images

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -7,7 +7,7 @@ import Inner from '../elements/Inner'
 const About = ({ children, offset }) => (
   <>
     <Divider bg="#23262b" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={`${offset}.1`} factor={1.2}/>
-    <Content speed={0.4} offset={`${offset}.2`} >
+    <Content speed={0.4} offset={`${offset}.3`} >
       <Inner>{children}</Inner>
     </Content>
   </>
